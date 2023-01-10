fa3a861060263e13c15dcce048b11dced199cb44
Liam Young <liam.young@canonical.com>
2023-01-10 09:38:15 +0000

    Run format and add unit test

--- integration_tests.py
+++ integration_tests.py
@@ -39,29 +39,29 @@ def test_service(snap: Snap, service: str):
 def test_config_set_unset(snap: Snap):
     # Unset the test keys before running the test proper just in case
     # it has been set by another test.
-    snap.config.unset(['foo', 'goo', 'test'])
+    snap.config.unset(["foo", "goo", "test"])
     with pytest.raises(UnknownConfigKey):
-        assert snap.config.get('foo')
+        assert snap.config.get("foo")
     with pytest.raises(UnknownConfigKey):
-        assert snap.config.get('goo')
-    snap.config.set({'foo': 'bar', 'goo': 'car'})
-    assert snap.config.get('foo') == 'bar'
-    assert snap.config.get('goo') == 'car'
-    snap.config.unset(['foo', 'goo'])
+        assert snap.config.get("goo")
+    snap.config.set({"foo": "bar", "goo": "car"})
+    assert snap.config.get("foo") == "bar"
+    assert snap.config.get("goo") == "car"
+    snap.config.unset(["foo", "goo"])
     with pytest.raises(UnknownConfigKey):
-        assert snap.config.get('foo')
+        assert snap.config.get("foo")
     with pytest.raises(UnknownConfigKey):
-        assert snap.config.get('goo')
+        assert snap.config.get("goo")
     # Check dotted notation correctly creates a dict
-    snap.config.set({'test.dotted': 'myvalue'})
-    assert snap.config.get('test') == {'dotted': 'myvalue'}
+    snap.config.set({"test.dotted": "myvalue"})
+    assert snap.config.get("test") == {"dotted": "myvalue"}
     # Check removing a key within the dict still leaves the
     # parent key
-    snap.config.unset(['test.dotted'])
-    assert snap.config.get('test') == {}
-    snap.config.set({'test.dotted': 'myvalue'})
+    snap.config.unset(["test.dotted"])
+    assert snap.config.get("test") == {}
+    snap.config.set({"test.dotted": "myvalue"})
     # Check removing the top level key removes the whole
     # entry.
-    snap.config.unset(['test'])
+    snap.config.unset(["test"])
     with pytest.raises(UnknownConfigKey):
-        snap.config.get('test')
+        snap.config.get("test")
