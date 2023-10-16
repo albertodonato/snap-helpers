e05640527b442b19a7b058a57fdf56678189c3f6
Alberto Donato <alberto.donato@gmail.com>
2023-10-16 09:09:47 +0200

    lints

--- snaphelpers/_hook.py
+++ snaphelpers/_hook.py
@@ -32,7 +32,7 @@ class Hook(NamedTuple):
         exists = True
         if not skip_load:
             try:
-                entry.load()  # type: ignore [no-untyped-call]
+                entry.load()
             except ModuleNotFoundError:
                 exists = False
         return cls(
